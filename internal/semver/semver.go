323ff2b800d7f661fc417396850269e75c5ffff6
Bård Aase <elzapp@gmail.com>
2021-02-09 09:20:14 +0100

    initial import

---
+++ internal/semver/semver.go
@@ -0,0 +1,136 @@
+package semver
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Semver is a struct representing a semeantic version
+type Semver struct {
+	Major      int
+	Minor      int
+	Patch      int
+	Prerelease string
+	Metadata   string
+}
+
+func (s *Semver) String() string {
+	ss := ""
+	ss = ss + fmt.Sprintf("%d.%d.%d", s.Major, s.Minor, s.Patch)
+	if s.Prerelease != "" {
+		ss = ss + fmt.Sprintf("-%s", s.Prerelease)
+	}
+	if s.Metadata != "" {
+		ss = ss + fmt.Sprintf("+%s", s.Metadata)
+	}
+	return ss
+}
+
+//ValidSemver checks if a semver string is valid
+func ValidSemver(ver string) (match bool) {
+	r := `^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$`
+	match, _ = regexp.Match(r, []byte(ver))
+	return
+}
+
+// ParseSemver takes a string representation of a semantic version,
+// and generates a Semver struct.
+func ParseSemver(ver string) (Semver, error) {
+	if !ValidSemver(ver) {
+		return Semver{}, fmt.Errorf("Invalid semver: %s", ver)
+	}
+	s := Semver{}
+	temp := strings.SplitN(ver, "+", 2)
+	ver = temp[0]
+	if len(temp) == 2 {
+		s.Metadata = temp[1]
+	}
+	temp = strings.SplitN(ver, "-", 2)
+	ver = temp[0]
+	if len(temp) == 2 {
+		s.Prerelease = temp[1]
+	}
+
+	temp = strings.Split(ver, ".")
+	s.Major = 0
+	s.Minor = 0
+	s.Patch = 0
+	if m, err := strconv.Atoi(temp[0]); err == nil {
+		s.Major = m
+	}
+	if m, err := strconv.Atoi(temp[1]); err == nil {
+		s.Minor = m
+	}
+
+	if m, err := strconv.Atoi(temp[2]); err == nil {
+		s.Patch = m
+	}
+	return s, nil
+}
+
+// BumpPatch takes a semver string, bumps the Patch version N.N.X up by one
+// clearing the prerel and metadata portions.
+func BumpPatch(ver string) (string, error) {
+	s, err := ParseSemver(ver)
+	if err != nil {
+		return "", err
+	}
+	s.Patch = s.Patch + 1
+	return s.String(), nil
+}
+
+// BumpMinor takes a semver string, bumps the Minor version N.X.0 up by one
+// and zeroes the patch version, and clearing the prerel and metadata.
+func BumpMinor(ver string) (string, error) {
+	s, err := ParseSemver(ver)
+	if err != nil {
+		return "", err
+	}
+	s.Minor = s.Minor + 1
+	s.Patch = 0
+	return s.String(), nil
+}
+
+// BumpMajor takes a semver string, bumps the Major version X.0.0 up by one
+// zeroing the minor and patch versions, as well as clearing the prerel and metadata
+func BumpMajor(ver string) (string, error) {
+	s, err := ParseSemver(ver)
+	if err != nil {
+		return "", err
+	}
+	s.Major = s.Major + 1
+	s.Minor = 0
+	s.Patch = 0
+	return s.String(), nil
+}
+
+// BumpPrerel takes a semver string, and bumps the prerel version.
+// If the prerel is unversioned (not numeric or ending in a .N)
+// the prerel version is set to 0.
+// 1.0.0.a2021 -> 1.0.0.a2021.0
+// 1.0.0.a2021.1 -> 1.0.0.a2021.2
+// 1.0.0 -> 1.0.0-0
+// 1.0.0-1 -> 1.0.0-2
+func BumpPrerel(ver string) (string, error) {
+	s, err := ParseSemver(ver)
+	if err != nil {
+		return "", err
+	}
+	if s.Prerelease == "" {
+		s.Prerelease = "0"
+		return s.String(), nil
+	}
+	parts := strings.SplitAfter(s.Prerelease, ".")
+	last := parts[len(parts)-1]
+	if prerelnum, err := strconv.Atoi(last); err == nil {
+		prerelnum = prerelnum + 1
+		parts[len(parts)-1] = fmt.Sprintf("%d", prerelnum)
+
+		s.Prerelease = strings.Join(parts, "")
+		return s.String(), nil
+	}
+	s.Prerelease = s.Prerelease + ".0"
+	return s.String(), nil
+}
---
+++ internal/semver/semver_test.go
@@ -0,0 +1,161 @@
+package semver_test
+
+import (
+	"semver/internal/semver"
+	"strings"
+	"testing"
+)
+
+func TestBumpPatch(t *testing.T) {
+	ver := "1.0.0"
+	expected := "1.0.1"
+	bumped, err := semver.BumpPatch(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+	strings.Compare("asd", "das")
+}
+
+func TestBumpPatchWithPrerelAndMetadata(t *testing.T) {
+	ver := "1.0.1-a20210131+myawesomebranch-a01f7f7a"
+	expected := "1.0.2-a20210131+myawesomebranch-a01f7f7a"
+	bumped, err := semver.BumpPatch(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+
+func TestBumpPrerelWithPrerelAndMetadata(t *testing.T) {
+	ver := "1.0.1-a20210131.1+myawesomebranch-a01f7f7a"
+	expected := "1.0.1-a20210131.2+myawesomebranch-a01f7f7a"
+	bumped, err := semver.BumpPrerel(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+
+func TestBumpPrerelWithDottedPrerelAndMetadata(t *testing.T) {
+	ver := "1.0.1-a2021013.1.1+myawesomebranch-a01f7f7a"
+	expected := "1.0.1-a2021013.1.2+myawesomebranch-a01f7f7a"
+	bumped, err := semver.BumpPrerel(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+func TestBumpPrerelWithUnversionedPrerelAndMetadata(t *testing.T) {
+	ver := "1.0.1-a20210131+myawesomebranch-a01f7f7a"
+	expected := "1.0.1-a20210131.0+myawesomebranch-a01f7f7a"
+	bumped, err := semver.BumpPrerel(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+func TestBumpPrerelWithoutPrerel(t *testing.T) {
+	ver := "1.0.1"
+	expected := "1.0.1-0"
+	bumped, err := semver.BumpPrerel(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+
+func TestBumpMinorWithPrerelAndMetadata(t *testing.T) {
+	ver := "1.0.1-a20210131+myawesomebranch-a01f7f7a"
+	expected := "1.1.0-a20210131+myawesomebranch-a01f7f7a"
+	bumped, err := semver.BumpMinor(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+
+func TestBumpMajorWithPrerelAndMetadata(t *testing.T) {
+	ver := "1.0.1-a20210131+myawesomebranch-a01f7f7a"
+	expected := "2.0.0-a20210131+myawesomebranch-a01f7f7a"
+	bumped, err := semver.BumpMajor(ver)
+	if err != nil {
+		panic(err)
+	}
+	if bumped != expected {
+		t.Errorf("PatchBumped version of '%s' was '%s', should've been '%s'", ver, bumped, expected)
+	}
+}
+
+func TestParseSemver(t *testing.T) {
+	ver := "1.0.1-a20210131+myawesomebranch-a01f7f7a"
+	expected := semver.Semver{Major: 1, Minor: 0, Patch: 1, Prerelease: "a20210131", Metadata: "myawesomebranch-a01f7f7a"}
+	actual, err := semver.ParseSemver(ver)
+	if err != nil {
+		panic(err)
+	}
+	if expected != actual {
+		t.Errorf("ParseSemver of '%s' produced '%+v', should've been '%+v'", ver, actual, expected)
+	}
+}
+func TestParseSemverNoPatch(t *testing.T) {
+	ver := "1.0-a20210131+myawesomebranch-a01f7f7a"
+	_, err := semver.ParseSemver(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+}
+
+func TestParseSemverEmpty(t *testing.T) {
+	ver := ""
+	_, err := semver.ParseSemver(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+}
+
+func TestParseSemverMajorOnly(t *testing.T) {
+	ver := "1"
+	_, err := semver.ParseSemver(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+	_, err = semver.BumpPatch(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+	_, err = semver.BumpMinor(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+	_, err = semver.BumpMajor(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+	_, err = semver.BumpPrerel(ver)
+	if err == nil {
+		t.Errorf("Version '%s' was accepted, though it is invalid", ver)
+
+	}
+
+}
